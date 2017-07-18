9962793ff72142c00efacc169fbf7006b32b3d74
Dmitry <CTAPOBEP@mail.ru>
2017-07-18 11:43:20 +0300

    [Backup::Bin] Обобщенный скрипт упаковки файлов в архив.

---
+++ bin/archiver.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem Using:
+rem 
+rem archiver Path\To\Source\*.txt Path\To\Result\Directory Password
+rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+set PathToSourceFiles=%~1
+set PathToResultDirectory=%~2
+set Password=%3
+
+set Time2Hour=%TIME: =0%
+set DateTime=%DATE:~-4%%DATE:~3,2%%DATE:~0,2%%Time2Hour:~0,2%%Time2Hour:~3,2%%Time2Hour:~6,2%
+
+set Options=-t7z -r -p%Password% -mhe
+set Exclude=-scsWIN -x@exclude.txt
+set PathToResultFile=%PathToResultDirectory%\backup%DateTime%.7z
+
+echo Archived...
+bin\7z.exe a %Options% %Exclude% "%PathToResultFile%" "%PathToSourceFiles%"
