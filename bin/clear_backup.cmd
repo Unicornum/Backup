06bd8b4e3b91581c231198fd8325e0a8adcfc916
Dmitry <unicornum.verum@gmail.com>
2021-06-02 09:13:11 +0300

    [Backup::Bin] fix: Скрипт очистки лога создания backup'a svn от лишних ревизий удаляет пустую строку в заголовке лога.

--- bin/clear_backup.cmd
+++ bin/clear_backup.cmd
@@ -13,13 +13,32 @@ set SrcBackupFile=%~1
 set DestBackupFile=%~2
 
 (
+
 FOR /F "tokens=1-3*" %%i IN (%SrcBackupFile%) DO (
-if "%%i %%j %%k" EQU "* Copied revision" (
-set max_revision=%%i %%j %%k %%l
-) else (
-if "!max_revision!" NEQ "" echo !max_revision!
-echo %%i %%j %%k %%l
-set max_revision=
+
+  if "%%i %%j %%k" EQU "* Copied revision" (
+    set max_revision=%%i %%j %%k %%l
+  ) else (
+  if "%%i %%j %%k" EQU "* Verifying repository" (
+    set dummy_revision=
+  ) else (
+  if "%%i %%j %%k" EQU "* Verifying metadata" (
+    set dummy_revision=
+  ) else (
+  if "%%i %%j %%k" EQU "* Verified revision" (
+    set max_verified_revision=%%i %%j %%k %%l
+  ) else (
+    if "!max_verified_revision!" NEQ "" echo !max_verified_revision!
+    if "!max_revision!" NEQ "" echo !max_revision!
+    echo %%i %%j %%k %%l
+    set max_revision=
+    set max_verified_revision=
+  ))))
+
+  if "%%i" EQU "Subject:" (
+    echo.
+  )
+
 )
-)
+
 ) > %DestBackupFile%
